3676a38d3a7232fd26450856e782719f22c6ebdd
René El Fatuaki <rene@elfatuaki.com>
2021-02-22 13:02:01 +0100

    for loop level1

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+//Lev1_1_js-vertiefung_loops_for-loop
+let text = "Hello World "
+for (let i = 1; i <= 10; i++) {
+    console.log(text + i)
+}
+
+//Lev1_2_js-vertiefung_loops
+let numArray = []
+for (let k = 0; k <= 10; k++) {
+    numArray.push(k)
+}
+console.log(numArray)
+
+//Lev1_4_js-vertiefung_loop
+let names = ["Georg", "Anass", "Elaine", "Hakan", "Sam", "Kim", "Sergio"]
+for (let y = 0; y < names.length; y++) {
+    console.log(names[y])
+}
